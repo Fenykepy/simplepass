02debf1f7cd36ce2702634e33722a172243b7b2f
Fenykepy <phiroom@lavilotte-rolle.fr>
2016-08-08 16:33:34 +0200

    update app actions.

--- src/public/app/actions.js
+++ src/public/app/actions.js
@@ -7,13 +7,20 @@ export function setDocumentTitle(title) {
   document.title = title + ' - SimplePass'
 }
 
+function setPassphrase(passphrase) {
+  return {
+    type: types.SET_PASSPHRASE,
+    passphrase
+  }
+}
+
 export function defineMasterPassphrase(passphrase) {
   return function(dispatch, getState) {
-    // set default json
     // encrypt json
     // store ejson in state
     // send ejson to server
     // store passphrase in state
+    dispatch(setPassphrase(passphrase))
     // launch timeout
   }
 }
