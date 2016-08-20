d00e37ea85de363b98e331d345068ace2239bf76
Fenykepy <pro@lavilotte-rolle.fr>
2016-08-20 14:35:17 +0200

    Fix issue #3
    Logout button now works with firefox.

--- src/public/user/containers/UserMenuButton.js
+++ src/public/user/containers/UserMenuButton.js
@@ -52,12 +52,12 @@ class UserMenuButton extends Component {
     } = this.props
 
     return (
-      <button
+      <a
         className="top-link dropdown-link"
         onClick={this.toogleMenu.bind(this)}
       ><div>{this.props.user.username}</div>
         {this.getMenu()}
-      </button>
+      </a>
     )
   }
 }
