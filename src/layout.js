4664e73b0f15b6d877ec8e0d3c4c725ced835339
Fenykepy <pro@lavilotte-rolle.fr>
2016-08-27 09:24:44 +0200

    Change favicon model.

--- src/layout.js
+++ src/layout.js
@@ -9,7 +9,7 @@ export default  `
         <meta name="description" content="SimplePass, the opensource password manager" />
         <meta name="author" content="Fenykepy" />
         <meta charset="utf-8" />
-        <link rel="icon" type="image/png" href="/static/images/favicon.png" />
+        <link rel="icon" type="image/png" href="/static/images/favicon.png?v=2" />
         <link rel="stylesheet" href="${webpackAssets.app.css}" />
       </head>
       <body>
