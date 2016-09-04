a842e5244f958d37b4a138bebaf03eab5f811810
Fenykepy <pro@lavilotte-rolle.fr>
2016-09-04 14:52:38 +0200

    Header successfully migrated to css modules.

--- src/public/user/components/user-menu/UserMenu.js
+++ src/public/user/components/user-menu/UserMenu.js
@@ -3,6 +3,7 @@ import React, { Component, PropTypes } from 'react'
 import PassphraseEditionButton from 'public/app/components/PassphraseEditionButton'
 import AdminLink from 'public/app/components/admin-link/AdminLink'
 import DropdownMenu from 'public/app/components/dropdown-menu/DropdownMenu'
+import DropdownList from 'public/app/components/dropdown-list/DropdownList'
 
 export default class UserMenu extends Component {
 
@@ -20,14 +21,14 @@ export default class UserMenu extends Component {
         <DropdownMenu
           close={this.props.close}
         >
-          <ul className="dropdown-menu">
+          <DropdownList>
             {this.getAdminLink()}
             <li><PassphraseEditionButton /></li>
             <li><a
                 href=""
                 onClick={this.props.logout}
             >Logout</a></li>
-          </ul>
+          </DropdownList>
         </DropdownMenu>
       </div>
     )
