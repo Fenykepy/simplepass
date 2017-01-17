7ec078f55d56af4a217dc8dbf511064800f45d17
Fenykepy <phiroom@lavilotte-rolle.fr>
2017-01-17 12:41:00 +0100

    migrate modalContent to css modules.

--- src/public/modal/components/modalContent/ModalContent.js
+++ src/public/modal/components/modalContent/ModalContent.js
@@ -1,12 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 
+import styles from './modalContent.less'
+
 export default class ModalContent extends Component {
 
   render() {
 
     return (
       <div
-        id="modal-content"
+        className={styles.modalContent}
       >
         {this.props.children}
       </div>
